1b797ac7d8c1e863caa028852deeb1b39f41d63d
Dhruv Bhatia <dhruv@dhruvbhatia.com>
2022-12-27 21:18:35 +1100

    initial commit

--- lib/todo_web/router.ex
+++ lib/todo_web/router.ex
@@ -1,6 +1,8 @@
 defmodule TodoWeb.Router do
   use TodoWeb, :router
 
+  import TodoWeb.UserAuth
+
   pipeline :browser do
     plug :accepts, ["html"]
     plug :fetch_session
@@ -8,6 +10,7 @@ defmodule TodoWeb.Router do
     plug :put_root_layout, {TodoWeb.Layouts, :root}
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug :fetch_current_user
   end
 
   pipeline :api do
@@ -15,16 +18,11 @@ defmodule TodoWeb.Router do
   end
 
   scope "/", TodoWeb do
-    pipe_through :browser
+    pipe_through [:browser]
 
     get "/", PageController, :home
 
     live "/todo_items", TodoItemLive.Index, :index
-    live "/todo_items/new", TodoItemLive.Index, :new
-    live "/todo_items/:id/edit", TodoItemLive.Index, :edit
-
-    live "/todo_items/:id", TodoItemLive.Show, :show
-    live "/todo_items/:id/show/edit", TodoItemLive.Show, :edit
   end
 
   # Other scopes may use custom stacks.
@@ -46,6 +44,50 @@ defmodule TodoWeb.Router do
 
       live_dashboard "/dashboard", metrics: TodoWeb.Telemetry
       forward "/mailbox", Plug.Swoosh.MailboxPreview
+    end
+  end
+
+  ## Authentication routes
+
+  scope "/", TodoWeb do
+    pipe_through [:browser, :redirect_if_user_is_authenticated]
+
+    live_session :redirect_if_user_is_authenticated,
+      on_mount: [{TodoWeb.UserAuth, :redirect_if_user_is_authenticated}] do
+      live "/users/register", UserRegistrationLive, :new
+      live "/users/log_in", UserLoginLive, :new
+      live "/users/reset_password", UserForgotPasswordLive, :new
+      live "/users/reset_password/:token", UserResetPasswordLive, :edit
+    end
+
+    post "/users/log_in", UserSessionController, :create
+  end
+
+  scope "/", TodoWeb do
+    pipe_through [:browser, :require_authenticated_user]
+
+    live_session :require_authenticated_user,
+      on_mount: [{TodoWeb.UserAuth, :ensure_authenticated}] do
+      live "/users/settings", UserSettingsLive, :edit
+      live "/users/settings/confirm_email/:token", UserSettingsLive, :confirm_email
+
+      live "/todo_items/new", TodoItemLive.Index, :new
+      live "/todo_items/:id/edit", TodoItemLive.Index, :edit
+
+      live "/todo_items/:id", TodoItemLive.Show, :show
+      live "/todo_items/:id/show/edit", TodoItemLive.Show, :edit
+    end
+  end
+
+  scope "/", TodoWeb do
+    pipe_through [:browser]
+
+    delete "/users/log_out", UserSessionController, :delete
+
+    live_session :current_user,
+      on_mount: [{TodoWeb.UserAuth, :mount_current_user}] do
+      live "/users/confirm/:token", UserConfirmationLive, :edit
+      live "/users/confirm", UserConfirmationInstructionsLive, :new
     end
   end
 end
